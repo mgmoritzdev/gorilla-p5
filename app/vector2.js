1b3d4a94265bce558dd958d95b13ef4b53e06392
Marcos Moritz <marcosmoritz@gmail.com>
2016-10-24 03:49:49 -0200

    Split files

---
+++ app/vector2.js
@@ -0,0 +1,12 @@
+var Vector2 = function(x, y) {
+  this.x = x;
+  this.y = y;
+};
+
+Vector2.prototype.dist = function(otherVector) {
+  return Math.pow(
+    Math.pow(
+      Math.abs(this.x - otherVector.x), 2) +
+      Math.pow(Math.abs(this.y - otherVector.y),2),
+    0.5);
+};
